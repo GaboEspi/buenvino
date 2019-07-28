a422b763b910e7b2124048c1a948e3e22b4285f3
Gabriel Espitia <gaesge@hotmail.com>
2019-07-28 03:41:05 -0500

    final 000webhost

--- conexion.php
+++ conexion.php
@@ -1,8 +1,3 @@
 <?php
-<<<<<<< HEAD
     $conexion = mysqli_connect("localhost", "id10315353_root", "12345678", "id10315353_buenvino") or die("ERROR en la conexion a DB: ".mysqli_errno($conexion)."<br>");
 ?>
-=======
-    $conexion = mysqli_connect("https://databases-auth.000webhost.com/index.php", "root", "12345678", "buenvino") or die("ERROR en la conexion a DB: ".mysqli_errno($conexion)."<br>");
-?>
->>>>>>> fb1421cde04e908eacccb26576dce10be59ecbd7
