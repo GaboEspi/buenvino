fb1421cde04e908eacccb26576dce10be59ecbd7
Gabriel Espitia Genes <39211961+GaboEspi@users.noreply.github.com>
2019-07-28 01:37:10 -0500

    servidor conexion 000webhost

--- conexion.php
+++ conexion.php
@@ -1,3 +1,3 @@
 <?php
-    $conexion = mysqli_connect("localhost", "root", "", "buenvino") or die("ERROR en la conexion a DB: ".mysqli_errno($conexion)."<br>");
+    $conexion = mysqli_connect("https://databases-auth.000webhost.com/index.php", "root", "12345678", "buenvino") or die("ERROR en la conexion a DB: ".mysqli_errno($conexion)."<br>");
 ?>
